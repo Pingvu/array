7d42815c38be9d303d708b556cddc91664060837
Johanness15 <jost04004@utb.vaxjo.se>
2021-09-21 14:06:53 +0200

    creat

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace yafa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] n= new int[5];
+            Console.WriteLine("5 nums");
+            for (int i = 0; i < n.Length; i++)
+            {
+                n[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 4; i < n.Length; i--)
+            {
+                Console.WriteLine(n[i]);
+            }
+           
+            
+        }
+    }
+}
